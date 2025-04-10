c06cd937e3415f0038e5bf264e59e5335b9b16ea
Henrique Braga <henrique.silva@duque.g12.br>
2025-04-10 20:58:38 -0300

    Segundo commit: criando as funções de adição e conclusão das tarefas.

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@ session_start();
 // Insira aqui os includes e/ou requires
 require_once __DIR__."/../src/controllers/controller.php";
 // Processa as requisições do navegador
-
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    handleRequest();
+}
 //exibir a lista de tarefas
 ?>
