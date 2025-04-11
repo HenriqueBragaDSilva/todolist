64d4fdf191661b7369aa8019ddf0b9584514e64e
Henrique Braga <henrique.silva@duque.g12.br>
2025-04-10 21:43:00 -0300

    Terceiro commit: criação do controller da aplicação

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -1,9 +1,23 @@
 <?php
+require_once __DIR__."/../"
 // Adicione aqui os includes e/ou requires
+
 
 // Função que manipula as requsições do servidor
 function handleRequest(){
     //Seu código aqui
+    if(isset($_POST['action'])){
+        switch($_POST['action']){
+            case 'add';
+            if(!empty($_POST['description'])){
+                addTask($_POST['description']);
+            }
+            break;
+        case 'toggle':
+            return
+            break;
+        }
+    }
 }
 
 // Função que exibe as tasks na view
